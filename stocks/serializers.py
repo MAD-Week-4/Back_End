7b280ca360bc8aa0eb7e3df84e30876e86ad4bab
sejun <sejun0601@naver.com>
2025-01-22 22:14:16 +0900

    Add view and serializer for user stock holdings retrieval
    Introduced `UserStockHoldingsView` to fetch user's stock holdings for a specific game. Added `UserStockHoldingSerializer` to handle serialization of these holdings. Updated URLs to include a new endpoint for accessing user stock data.

--- stocks/serializers.py
+++ stocks/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import StockSymbol, StockDailyData, TradeLog, AiTradeLog
+from .models import StockSymbol, StockDailyData, TradeLog, AiTradeLog, UserStockHolding
 
 class StockSymbolSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,3 +29,15 @@ class AiTradeLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = AiTradeLog
         fields = ['game','stock', 'date', 'price', 'quantity', 'is_buy', 'created_at', 'profit']
+
+class UserStockHoldingSerializer(serializers.ModelSerializer):
+    """
+    UserStockHolding 모델을 JSON으로 직렬화하는 Serializer
+    """
+    user = serializers.CharField(source='user.username', read_only=True)  # 유저 이름 표시
+    game = serializers.CharField(source='game.name', read_only=True)  # 게임 이름 표시
+    stock = serializers.CharField(source='stock.name', read_only=True)  # 주식 종목 이름 표시
+
+    class Meta:
+        model = UserStockHolding  # 직렬화할 모델 지정
+        fields = ['user', 'game', 'stock', 'quantity']  # 노출할 필드 지정
